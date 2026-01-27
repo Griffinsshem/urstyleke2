95424d185dd800d7e34d3f081ace3dda2cb9de10
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-01-27 10:42:09 +0300

    chore(app): register auth blueprint

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from auth import auth_bp
 
 app = Flask(__name__)
 CORS(app)
 
+# Register blueprints
+app.register_blueprint(auth_bp)
+
 @app.route("/")
 def home():
     return jsonify({"message": "UrStyleKE API running 🚀"})
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    
