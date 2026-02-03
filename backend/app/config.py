f13d7328de802d6fb4c0d2bd088bb15e2cd12422
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-02-03 08:48:32 +0300

    refactor: switch database config to SQLite for local development

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,14 +5,20 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:"
-        f"{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
-    )
+    DB_TYPE = os.getenv("DB_TYPE", "sqlite")
+
+    if DB_TYPE == "sqlite":
+        SQLALCHEMY_DATABASE_URI = "sqlite:///urstyleke.db"
+    else:
+
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql://{os.getenv('DB_USER')}:"
+            f"{os.getenv('DB_PASSWORD')}@"
+            f"{os.getenv('DB_HOST')}:"
+            f"{os.getenv('DB_PORT')}/"
+            f"{os.getenv('DB_NAME')}"
+        )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
