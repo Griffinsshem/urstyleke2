286e8c35025e7a648f397ca338421e3a67525e6f
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-02-03 16:57:13 +0300

    feat(auth): integrate JWT authentication and update user schema

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,31 +1,22 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
-from app.config import Config
-from app.extensions import db, migrate, jwt
+from .config import Config
+from .extensions import db, migrate
+
+
+jwt = JWTManager()
 
 
 def create_app():
-
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    CORS(app)
-
 
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    from app import models
-
-    # Register blueprints
-    from app.routes.auth import auth_bp
-
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-
-    @app.route("/")
-    def home():
-        return {"message": "UrStyleKE API running 🚀"}
+    from .routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
