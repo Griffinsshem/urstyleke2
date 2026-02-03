bf767f8f1234d5c2ee997c5ce1c34addbb4bafb7
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-02-03 15:51:28 +0300

    Configure JWT authentication and initialize JWT manager

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-dev-secret")
 
     DB_TYPE = os.getenv("DB_TYPE", "sqlite")
 
